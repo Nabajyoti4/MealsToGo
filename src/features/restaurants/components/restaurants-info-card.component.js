507f0d15b243ed18b97e0abd1b983bfb81a57788
Nabajyoti4 <sirnaba@gmail.com>
2021-10-24 18:38:22 +0530

    text compoent and style file for restauratn info card added

--- src/features/restaurants/components/restaurants-info-card.component.js
+++ src/features/restaurants/components/restaurants-info-card.component.js
@@ -1,57 +1,19 @@
 import React from "react";
-import { Image, Text, View } from "react-native";
-import { Card } from "react-native-paper";
-import styled from "styled-components/native";
+import { Image, View } from "react-native";
 import { SvgXml } from "react-native-svg";
 import star from "../../../../assets/star";
 import open from "../../../../assets/open";
 import { Spacer } from "../../../components/spacer/spacer.component";
+import { Text } from "../../../components/typography/text.component";
 
-const RestaurantCard = styled(Card)`
-  background-color: ${(props) => props.theme.colors.bg.primary};
-`;
-
-const RestaurantCardCover = styled(Card.Cover)`
-  padding: ${(props) => props.theme.space[3]};
-  background-color: ${(props) => props.theme.colors.bg.primary};
-`;
-
-const Title = styled(Text)`
-  font-family: ${(props) => props.theme.fonts.heading};
-  font-size: ${(props) => props.theme.fontSizes.body};
-  color: ${(props) => props.theme.colors.ui.primary};
-`;
-
-const Address = styled(Text)`
-  font-family: ${(props) => props.theme.fonts.body};
-  font-size: ${(props) => props.theme.fontSizes.caption};
-  color: ${(props) => props.theme.colors.ui.primary};
-`;
-
-const Info = styled.View`
-  padding: ${(props) => props.theme.space[3]};
-`;
-
-const Rating = styled.View`
-  flex-direction: row;
-  padding-top: ${(props) => props.theme.space[2]};
-  padding-bottom: ${(props) => props.theme.space[2]};
-`;
-
-const Section = styled.View`
-  flex-direction: row;
-  align-items: center;
-`;
-
-const SectionEnd = styled.View`
-  flex: 1;
-  flex-direction: row;
-  justify-content: flex-end;
-`;
-
-const Open = styled(SvgXml)`
-  flex-direction: row;
-`;
+import {
+  RestaurantCard,
+  RestaurantCardCover,
+  Info,
+  Section,
+  SectionEnd,
+  Rating,
+} from "./restaurants-info-card.style";
 
 export const RestaurantInfo = ({ restaurant = {} }) => {
   const {
@@ -73,7 +35,7 @@ export const RestaurantInfo = ({ restaurant = {} }) => {
       <RestaurantCard elevation={5}>
         <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
         <Info>
-          <Title>{name}</Title>
+          <Text variant="label">{name}</Text>
           <Section>
             <Rating>
               {ratingArray.map(() => (
@@ -82,9 +44,7 @@ export const RestaurantInfo = ({ restaurant = {} }) => {
             </Rating>
             <SectionEnd>
               {isClosedTemporarily && (
-                <Text variant="label" style={{ color: "red" }}>
-                  CLOSED TEMPORARILY
-                </Text>
+                <Text variant="error">CLOSED TEMPORARILY</Text>
               )}
 
               <Spacer position="left" size="large"></Spacer>
@@ -94,7 +54,7 @@ export const RestaurantInfo = ({ restaurant = {} }) => {
             </SectionEnd>
           </Section>
 
-          <Address>{address}</Address>
+          <Text>{address}</Text>
         </Info>
       </RestaurantCard>
     </View>
